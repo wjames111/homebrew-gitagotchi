3fc0b82a389c9519bd0099eca811310530ce0398
gitagotchi release <wjames111@users.noreply.github.com>
2026-07-16 19:15:45 +0000

    gitagotchi 2.0.0

--- Formula/gitagotchi.rb
+++ Formula/gitagotchi.rb
@@ -1,8 +1,8 @@
 class Gitagotchi < Formula
   desc "Terminal Tamagotchi whose entire life is derived from your GitHub account"
   homepage "https://github.com/wjames111/gitagotchi"
-  url "https://github.com/wjames111/gitagotchi/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "a03574a44d67b577386551321c9f79157349207768ce8cfbec673f0c63f29661"
+  url "https://github.com/wjames111/gitagotchi/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "d145d875ef6a40be0c2c43b4e054ee795f2f4455d17d688011a2fdae2ddb0eb8"
   license "MIT"
 
   depends_on "bash"
